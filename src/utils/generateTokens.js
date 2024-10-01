ccaef7ba8c7bc74161479bf30048ead66f2b6905
GauravKesh <gkrcoder@gmail.com>
2024-10-01 14:13:22 +0530

    "jwt token added"

--- src/utils/generateTokens.js
+++ src/utils/generateTokens.js
@@ -6,13 +6,6 @@ const generateToken = ( userData) => {
   const payload = {userData };
 
   const token = jwt.sign(payload, secretKey, { expiresIn: "30d" });
-
-  // res.cookie("jwt", token, {
-  //   httpOnly: true,
-  //   secure: process.env.NODE_ENV !== "development",
-  //   sameSite: "strict",
-  //   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day in milliseconds
-  // });
   return token;
 };
 module.exports = {generateToken};
